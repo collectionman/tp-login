3184ea5523efecf76833403bda39fee2de2ba544
collectionman <julianllorensi@outlook.com>
2019-07-14 04:09:57 -0300

    Commit-1: First basic version works

--- src/version_0/js/user-login-logic.js
+++ src/version_0/js/user-login-logic.js
@@ -1,8 +1,38 @@
+var users = [
+    {
+        username: 'Paul',
+        password: '9dg67bskcnadlop',
+        permission: 'ADMIN' 
+    },
+    {
+        username: 'Steve',
+        password: 'owiu992bdsd11po',
+        permission: 'COMMON'
+    }
+] 
+
 let usernameTextField = document.getElementById('username-text-field') ;
 let passwordTextField = document.getElementById('password-text-field') ;
 
 let loginButton = document.getElementById('login-button') ;
-loginButton.addEventListener('click', () => {
-    console.log('username: ' + usernameTextField.textContent.toString()) ;
-    console.log('password: ' + passwordTextField.textContent.toString()) ;
+loginButton.addEventListener('click', function() {
+    const username = usernameTextField.value ;
+    const password = passwordTextField.value ;
+
+    users.forEach( user => {
+        if (user.username == username && user.password == password) {
+            console.log(`You're logged as ${user.permission}`) ;
+        } else {
+            console.log(`That user doesn't exists`) ;
+            addNewUserAsCommon({username: username, 
+                                password: password, 
+                                permission: 'COMMON'}) ;
+        }    
+    }) ;
 }) ;
+
+function addNewUserAsCommon(user) {
+    users.push({username: user.username, 
+                password: user.password, 
+                permission: user.permission}) ;
+}
